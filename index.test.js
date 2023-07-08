23e3d20865015c013a303577fb02a7fa7260e355
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-07-08 13:31:32 +0200

    FEAT: Test

---
+++ index.test.js
@@ -0,0 +1,38 @@
+console.log("/**************************************/");
+console.log("/** Json Server Test | index.test.js **/");
+console.log("/**************************************/\n");
+
+const fetchData = async () => {
+  try {
+    const urls = [
+      "http://localhost:4444/pokemons",
+      "http://localhost:4444/counts",
+    ];
+    const responses = await Promise.all(
+      urls.map(async (url) => {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(
+            `Failed to fetch data from ${url}: ${response.status} - ${response.statusText}`
+          );
+        }
+        try {
+          const data = await response.json();
+          return { url, data };
+        } catch (error) {
+          throw new Error(`Error parsing JSON response from ${url}: ${error}`);
+        }
+      })
+    );
+    responses.map((resp) => {
+      if (!resp.data || !resp.data.length)
+        throw new Error(`No data fetched from ${resp.url}`);
+    });
+    console.log("All tests passed! on : ");
+    console.table(urls);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchData();
